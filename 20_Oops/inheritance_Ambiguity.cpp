72bf8384965e922ff7f1b396090e9df912b8866d
Jaryaldikshant <dikshant.jaryal08@gmailcom>
2024-02-14 13:35:33 +0530

    ambiguity

---
+++ 20_Oops/inheritance_Ambiguity.cpp
@@ -0,0 +1,41 @@
+
+// if we have function func1() with same name in two diffent class A and B and class C in inherit form A and B
+// , so during function call be use scope resolution function ::
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    void print()
+    {
+        cout << "Hello!!" << endl;
+    }
+};
+
+class B
+{
+
+public:
+    void print()
+    {
+        cout << "Hey" << endl;
+    }
+};
+
+class C : public A, public B
+{
+};
+
+int main()
+{
+    C obj1;
+
+    // here (::) scope resolution is used to know function of which class is to be called
+    obj1.A::print();
+
+    obj1.B::print();
+
+    return 0;
+}
