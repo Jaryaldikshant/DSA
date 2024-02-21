12a89f0aa7131caf46aa5b49187faec34d2260bf
Jaryaldikshant <dikshant.jaryal08@gmailcom>
2024-02-21 17:39:25 +0530

    Insertion and deletion is linked list

---
+++ 21_Linked_List/Insertion_Deletion_inLinked/kth_Value_deletion.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL;
+    }
+};
+
+Node *convertLink(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+
+    Node *mover = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node *removeK(Node *head, int val)
+{
+    if(head == NULL) return head;
+
+    if(head->data == val){
+        Node* temp = head;
+        head = head->next;
+        delete(temp);
+        return head;
+    }
+
+    Node* temp = head;
+    Node* prev = NULL;
+
+    while( temp != NULL){
+        if(temp->data == val){
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+int main()
+{
+    vector<int> arr = {12, 6, 2, 9};
+    Node *head = convertLink(arr);
+
+    head = removeK(head,6);
+    print(head);
+    return 0;
+}
